a38c0f768011010d0a3e65b666a82a4751f67515
âyejianghuai <921870425@qq.com>
2020-12-08 17:30:22 +0800

    下班前提交

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,15 +11,19 @@
         <span class="iconfont left link"></span>
         <span class="iconfont right link marginLeft16px"></span>
       </div>
-      <div>
+      <div class="keyword-wrap">
         <el-input
-          placeholder="请输入内容"
-          v-model="input3"
-          class="input-with-select"
-        >
-          <el-button slot="append" icon="el-icon-search"></el-button>
-        </el-input>
+          prefix-icon="iconfont search"
+          v-model="keyword"
+          placeholder="搜索"
+          size="mini"
+          :class="onFocus ? 'focus' : 'el-input'"
+          @focus="changeSize"
+          @blur="changeSize"
+        ></el-input>
+        <span class="link iconfont maikefeng" title="听歌识曲"></span>
       </div>
+      <div></div>
     </div>
   </div>
 </template>
@@ -29,6 +33,11 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({ components: {} })
 export default class Header extends Vue {
   imgUrl: string = require("@/assets/wyy.png");
+  keyword = "";
+  onFocus = false;
+  changeSize() {
+    this.onFocus = !this.onFocus;
+  }
 }
 </script>
 
@@ -43,8 +52,11 @@ export default class Header extends Vue {
   z-index: 100;
   box-sizing: border-box;
   color: #fff;
+
   .container {
-    display: flex;
+    grid-template-columns: 200px 50px 300px 1fr; /*定义多少列，多宽*/
+    display: grid;
+    column-gap: 8px; /*列的间距*/
     .logo {
       height: 28px;
       img {
@@ -53,8 +65,26 @@ export default class Header extends Vue {
       }
     }
     .next-or-pre {
-      margin-left: 100px;
       color: #fff;
+    }
+    .keyword-wrap {
+      .el-input {
+        width: 80px !important;
+        transition: all 0.5s ease 0s !important;
+      }
+      .focus {
+        width: 240px !important;
+        transition: all 0.5s ease 0s !important;
+      }
+      .maikefeng {
+        display: inline-block;
+        margin-left: 8px;
+        width: 36px;
+        text-align: center;
+        border-radius: 100%;
+        font-size: 18px;
+        background: #565656;
+      }
     }
   }
 }
--- src/js/init.ts
+++ src/js/init.ts
@@ -1,5 +1,6 @@
 import "animate.css";
 import "@/css/common.scss";
+import "element-ui/lib/theme-chalk/index.css";
 import { prototypeInit } from "./prototypeInit";
 import { VueConstructor } from "vue/types/umd";
 
